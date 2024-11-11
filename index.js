3188f46c914e727a8f2dde39f13573e4caec0ec7
Mayur Kamble <mayurkamble0250@gmail.com>
2024-11-11 15:25:09 +0530

    added post method to add new product to db

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
+const Product = require("./models/product.model.js");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
 const mongoURI = process.env.MONGODB_CONNECT;
